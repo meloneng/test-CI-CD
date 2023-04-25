5105641fe13a38b9d65084c84f4dda7e2e91eb38
meloneng <melon_eng@protonmail.com>
2023-04-25 18:29:09 -0300

    simple java pipeline completed

--- java-test/HelloWorld.java
+++ java-test/HelloWorld.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class HelloWorld{
     
     public static void main(String[] args){
-        System.out.println("\n Hello World ");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nHello World\nSay your name:");
+        String name = scanner.nextLine();
+        System.out.println("\nHello " + name + "\n");
     }
 }
