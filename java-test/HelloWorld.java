28918ca8e6cd937b492092e0df2e8fc4179e6c25
meloneng <melon_eng@protonmail.com>
2023-04-25 19:26:36 -0300

    .

--- java-test/HelloWorld.java
+++ java-test/HelloWorld.java
@@ -5,8 +5,9 @@ public class HelloWorld{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\nHello World\nSay your name:");
-        String name = scanner.nextLine();
-        System.out.println("\nHello " + name + "\n");
+        System.out.println("\nHello World\nSay your name:\n");
+        String name = "Doubolu";
+        System.out.println("\nIt's Danboulu, not to be confused with Doubolu\n");
+        System.out.println("Hello " + name + "\n");
     }
 }
